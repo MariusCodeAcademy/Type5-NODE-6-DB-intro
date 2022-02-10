9ed6b318c83783010d8c0e7aea96955c1dcc0a98
Marius <marius@example.com>
2022-02-10 14:56:58 +0200

    book post

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -3,7 +3,11 @@ const { successResponce } = require('../helpers/dbHelpers');
 async function booksIndex(req, res) {
   successResponce(res, 'hello from booksIndex');
 }
+async function addBook(req, res) {
+  successResponce(res, req.body);
+}
 
 module.exports = {
   booksIndex,
+  addBook,
 };
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -5,5 +5,6 @@ const booksController = require('../controllers/booksController');
 
 router.get('/api/books', booksController.booksIndex);
 // POST /api/books/add (grazina ka gauna i req.body)
+router.post('/api/books/add', booksController.addBook);
 
 module.exports = router;
