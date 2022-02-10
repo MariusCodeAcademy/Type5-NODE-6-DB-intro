f3d12582796435e3e4fdb83d8489a8af8bbd6cef
Marius <marius@example.com>
2022-02-10 15:22:41 +0200

    duomenys

---
+++ src/models/authorModel.js
@@ -0,0 +1,13 @@
+// database name Library
+// Authors, Books
+
+const author = [
+  { name: 'Author James', gender: 'male', country: 'Africa', books: [] },
+  { name: 'Author Jill', gender: 'female', country: 'UK', books: [] },
+  { name: 'Author Katherine', gender: 'female', country: 'Europe', books: [] },
+];
+// db.authors.insertMany([
+//   { name: 'Author James', gender: 'male', country: 'Africa', books: [] },
+//   { name: 'Author Jill', gender: 'female', country: 'UK', books: [] },
+//   { name: 'Author Katherine', gender: 'female', country: 'Europe', books: [] }
+// ])
---
+++ src/models/booksModel.js
@@ -0,0 +1,10 @@
+const books = [
+  { title: 'book1', year: 2000, rating: 3, author: id },
+  { title: 'book2', year: 2001, rating: 2, author: id },
+  { title: 'book3', year: 2002, rating: 3, author: id },
+  { title: 'book4', year: 2015, rating: 2, author: id },
+  { title: 'book5', year: 2020, rating: 3, author: id },
+  { title: 'book6', year: 2011, rating: 1, author: id },
+];
+
+// id naudosim tuos kuriuos gausim is DB sukure autorius
