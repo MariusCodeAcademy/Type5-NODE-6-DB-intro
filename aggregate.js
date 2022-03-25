f625be90192d20cc53f49e7700a271a6d3ec2e88
Marius <marius@example.com>
2022-03-25 11:22:51 +0200

    agg

--- aggregate.js
+++ aggregate.js
@@ -11,14 +11,46 @@ db.books.aggregate([
 ]);
 
 // rasti kiekvieno ratingo metu suma
-db.books.aggregate([
+
+const pipeline = [
   { $match: {} },
   {
     $group: {
       _id: '$rating',
-      avgYearPerRating: { $sum: '$year' },
+      avgSumOfYEARPerRating: { $sum: '$year' },
       bookCountPerRating: { $sum: 1 },
     },
   },
   { $sort: { bookCountPerRating: -1 } },
-]);
+];
+db.books.aggregate(pipeline);
+
+
+/**
+ * from: The target collection. su kokia lentele jungiam
+ * localField: The local join field. vietinis laukas kuris yra vienoda su kitos lenteles lauku
+ * foreignField: The target join field.
+ * as: The name for the results.
+ * pipeline: The pipeline to run on the joined collection.
+ * let: Optional variables to use in the pipeline field stages.
+ */
+ {
+  from: 'authors',
+  localField: 'author',
+  foreignField: '_id',
+  as: 'authorObj'
+}
+// authorName: authorObj[0].name
+
+db.books.aggregate([
+  {
+    '$match': {}
+  }, {
+    '$lookup': {
+      'from': 'authors', 
+      'localField': 'author', 
+      'foreignField': '_id', 
+      'as': 'authorArr'
+    }
+  }
+])
